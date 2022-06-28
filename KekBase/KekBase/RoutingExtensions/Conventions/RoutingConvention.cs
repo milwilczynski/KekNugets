b9a5230c0875e2249c65f2505bc3265b50d4788b
Miłosz Wilczyński <mwilcz08@gmail.com>
2022-06-28 19:09:34 +0200

    RoutingConvention to create routing based on configuration

---
+++ KekBase/KekBase/RoutingExtensions/Conventions/RoutingConvention.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using KekBase.BaseConfigurations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace KekBase.RoutingExtensions.Conventions;
+
+public class RoutingConvention : IControllerModelConvention
+{
+    private readonly string _apiVersion;
+    private readonly string _domain;
+
+    public RoutingConvention(AppConfiguration configuration)
+    {
+        _domain = configuration.Domain;
+        _apiVersion = configuration.ApiVersion;
+    }
+
+    public void Apply(ControllerModel controller)
+    {
+        //without skipping RouteAnalyzer swagger dosen't work, sadge
+        if (controller.ControllerName == "RouteAnalyzer_Main")
+        {
+            return;
+        }
+
+        var hasRouteAttributes = controller.Selectors.Any(selector =>
+            selector.AttributeRouteModel != null);
+        if (hasRouteAttributes)
+        {
+            return;
+        }
+
+        var namespc = controller.ControllerType.Namespace;
+        if (namespc == null)
+        {
+            return;
+        }
+
+        var template = new StringBuilder();
+        template.Append($"/api/v{_apiVersion}/{_domain}");
+        template.Append("/[controller]/[action]");
+
+        foreach (var selector in controller.Selectors.ToList())
+        {
+            selector.AttributeRouteModel =
+                AttributeRouteModel.CombineAttributeRouteModel(
+                    new AttributeRouteModel(new RouteAttribute(template.ToString())),
+                    selector.AttributeRouteModel);
+        }
+    }
+}
---
+++ KekBase/KekBase/RoutingExtensions/RoutingInitalizer.cs
@@ -0,0 +1,19 @@
+﻿using KekBase.BaseConfigurations;
+using KekBase.RoutingExtensions.Conventions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KekBase.RoutingExtensions;
+
+public static class RoutingInitalizer
+{
+    public static void RoutingInitializer(IServiceCollection services, IConfiguration configuration)
+    {
+        var config = new AppConfiguration();
+        configuration.GetSection("AppConfiguration").Bind(config);
+
+        services.AddControllers(options => options.Conventions.Add(
+            new RoutingConvention(config)));
+        services.AddRouting();
+    }
+}
